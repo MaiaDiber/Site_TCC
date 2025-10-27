38f7d5b41b151960a11cea0d7776712f5818edf3
Gustavo <ra50350929840@acaonsfatima.org.br>
2025-10-27 16:40:49 -0300

    cadastro

--- FRONTEND/src/components/Cadastro/cabecalhoCadastro.jsx
+++ FRONTEND/src/components/Cadastro/cabecalhoCadastro.jsx
@@ -7,9 +7,11 @@ export default function CabecalhoCadastro() {
              <img className='logosite' src="/images/logo_ViaSaúde.png" height={150} alt="" />
 
              <div className="aviso">
-                <img src="/images/ChatGPT_Image_22_de_out._de_2025__23_34_59-removebg-preview.png" alt="" height={50} />
+                <img src="/images/ChatGPT_Image_22_de_out._de_2025__23_34_59-removebg-preview.png" alt="" height={35} />
 
-                <p><strong>Atenção: </strong>Este é apenas um pré-cadastro. Você ainda precisará comparecer à unidade de saúde com seus documentos originais (RG, CPF e comprovante de residência) para finalizar o cadastro e receber o Cartão SUS.</p>
+                <p><strong>Atenção: </strong>Este é apenas um pré-cadastro. Você ainda precisará comparecer à unidade de <br />
+                 saúde com seus documentos originais (RG, CPF e comprovante de residência) para finalizar <br />
+                  o cadastro e receber o Cartão SUS.</p>
              </div>
         </section>
         </>
--- FRONTEND/src/pages/Cadastro/Cadastrao.jsx
+++ FRONTEND/src/pages/Cadastro/Cadastrao.jsx
@@ -13,6 +13,73 @@ export default function Cadastro() {
         <CabecalhoCadastro />
 
 
+            <div className="primeiroP">
+                <p>Preencha os dados abaixo para agilizar seu atendimento na unidade de saúde</p>
+            </div>
+
+            <div className="informação-usuário">
+               <div className="dados-usu">
+               <p>Dados pessoais</p>
+               </div>
+
+                <div className="dados">
+                    <label>  <p>Nome Completo*</p>
+                        <input type="text" />
+                    </label>
+
+                    <div className="grupo1">
+                        <label> <p>CPF*</p>
+                            <input type="text" />
+                            </label>
+                            <label> <p>Data de Nascimeto*</p>
+                                <input type="text" />
+                                </label>
+                    </div>
+                
+                <label > <p>Telefone*</p>
+                    <input type="text" />
+                </label>
+                </div>
+
+                <div className="endereço">
+                <div className="endereço-usu">
+               <p>Endereço</p>
+               </div>
+
+                <label> <p>CEP*</p>
+                    <input type="text" />
+                </label>
+
+                <div className="grupo2">
+                        <label> <p>Rua/Avenida*</p>
+                            <input type="text" />
+                            </label>
+                            <label> <p>Número*</p>
+                                <input type="text" />
+                                </label>
+                    </div>
+
+                    <label> <p>Bairro*</p>
+                        <input type="text" />
+                    </label>
+
+                    <div className="grupo3">
+                        <label> <p>Cidade*</p>
+                            <input type="text" />
+                            </label>
+                            <label> <p>Estado</p>
+                                <input type="text" />
+                                </label>
+                    </div>
+                </div>
+
+                <div className="doisfinais">
+                    <button type='button' >Cadarstra-se</button>
+                    <p>* Campos obrigratórios</p>
+                </div>
+            </div>
+
+
     </section>
     </section>
     </>
