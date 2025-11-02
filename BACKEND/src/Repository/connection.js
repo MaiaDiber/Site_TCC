0001ffcbe4c40721da6b1053f0a53cf1b059cdcc
Shoe-lucas <lucasmatosplaygo@gmail.com>
2025-11-02 20:07:11 -0300

    eu fiz uma mudança no backend, a rotas.js, no connection.js eu ainda n fiz as API mas dei umas mudadinhas de leve mas vou só ver se deu certo antes de continuar acho q fiz o commit certo hehehaw

--- BACKEND/src/Repository/connection.js
+++ BACKEND/src/Repository/connection.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql2/promise'
 
-const conexao = await mysql.createConnection({
+const conexao =  mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     port: process.env.MYSQL_PORT,
--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import conexao from './Repository/connection.js';
+import rotas  from './rotas.js'
+
 const servidor = express();
 servidor.use(express.json());
 servidor.use(cors());
