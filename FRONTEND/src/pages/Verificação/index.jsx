783a411b35ae0a739cc870cae73fdce12aea58a8
Luigi0012 <ra50242081886@acaonsfatima.org.br>
2025-11-03 16:40:21 -0300

    fgffgffff

---
+++ FRONTEND/src/pages/Verificação/index.jsx
@@ -0,0 +1,25 @@
+import './index.scss'
+
+export default function Verificacao() {
+
+    return(
+        <>
+            <section className=''>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </section>
+        </>
+    )
+}
