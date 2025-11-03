c554b6af11a26eb10a54383b9e1f73a1131887e1
Gustavo <ra50350929840@acaonsfatima.org.br>
2025-11-03 15:28:29 -0300

    rtyrttttttttt

--- FRONTEND/src/components/Cadastro/cabecalhoCadastro.jsx
+++ FRONTEND/src/components/Cadastro/cabecalhoCadastro.jsx
@@ -1,4 +1,5 @@
 import './cabecalhoCadastro.scss'
+import { Link } from 'react-router'
 
 export default function CabecalhoCadastro() {
     return (
@@ -7,7 +8,13 @@ export default function CabecalhoCadastro() {
              <img className='logosite' src="/assets/Images/logo_ViaSaúde.png" height={150} alt="" />
 
              <div className="aviso">
+
+                <div className="duo">
+                    <Link to={'/Entrar'}> 
+                    <img src="arrow_left_circle_icon-icons.com_66702.png" height={20} alt="" />
+                    </Link>
                 <img src="/assets/Images/ChatGPT_Image_22_de_out._de_2025__23_34_59-removebg-preview.png" alt="" height={30} />
+                </div>
 
                 <p><strong>Atenção: </strong>Este é apenas um pré-cadastro. Você ainda precisará comparecer à unidade de <br />
                  saúde com seus documentos originais (RG, CPF e comprovante de residência) para finalizar <br />
---
+++ FRONTEND/src/pages/Entrar/Entrar.jsx
@@ -0,0 +1,10 @@
+import './Entrar.scss'
+
+
+export default function Entrar() {
+    return (
+        <>
+        <h1>testando</h1>
+        </>
+    )
+}
