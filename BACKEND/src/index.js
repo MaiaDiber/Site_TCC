06609afe5d7229b6ebd7f5a3e08e5db1743bd672
Shoe-lucas <lucasmatosplaygo@gmail.com>
2025-11-05 11:55:18 -0300

    eu fiz algumas mudanças mas o vs code ta de sacanagem e n quer colocar as outras

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -3,10 +3,14 @@ import express from 'express'
 import cors from 'cors'
 import conexao from './Repository/connection.js';
 import rotas  from './rotas.js'
+import usuarioController from './controller/usuarioController.js';
 
 const servidor = express();
 servidor.use(express.json());
 servidor.use(cors());
 
+servidor.use(usuarioController);
 
-servidor.listen(process.env.PORTA, () => console.log(`---A porta ${process.env.PORTA} subiu com sucesso`))
+servidor.listen(process.env.PORTA, () => 
+    console.log(`---A porta ${process.env.PORTA} subiu com sucesso`)
+);
