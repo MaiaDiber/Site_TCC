2c5fc5feea5472f3dcede23d89ab72b6ed14a195
Shoe-lucas <lucasmatosplaygo@gmail.com>
2025-11-05 18:23:47 -0300

    mais coisas diferentes

--- BACKEND/src/controller/usuarioController.js
+++ BACKEND/src/controller/usuarioController.js
@@ -20,6 +20,20 @@ router.post('/usuarios', async (req, res) => {
             estado
         } = req.body;
 
+          const  dataNasc = new Date(data_nascimento);
+          const dataMinima = new Date('1900-01-01');
+          const dataAtual = new Date();
+
+          if (isNaN(dataNasc.getTime())){
+            return res.status(400).send({erro: 'Data de nascimento invalida.'});
+          }
+
+          if (dataNasc < dataMinima || dataNasc > dataAtual){
+            return res.status(400).send({
+              erro: 'Data de nascimento deve estar entre 01/01/1900 e a data atual.'
+          });
+          }
+
         const [resultadoUsuario] = await conexao.execute(
             `INSERT INTO usuarios (nome_completo, cpf, data_nascimento, telefone, email, senha)
             VALUES (?, ?, ?, ?, ?, ?)`,
