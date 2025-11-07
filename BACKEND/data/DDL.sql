707e1dfc521c5d57980b4eb57152f5dc574887fd
Shoe-lucas <lucasmatosplaygo@gmail.com>
2025-11-07 12:37:10 -0300

    mudancas uiii

---
+++ BACKEND/data/DDL.sql
@@ -0,0 +1,40 @@
+drop database if exists TCC;
+
+create database TCC;
+use TCC;
+
+create table Cadastrar(
+id int primary key auto_increment,
+nome_completo varchar(100) not null,
+cpf varchar(100) not null,
+data_nascimento date not null,
+senha varchar(100) not null,
+email varchar(200) not null,
+tipo enum('Paciente', 'Adm') default 'Paciente',
+id_endereco int not null,
+id_campanha int not null,
+foreign key (id_endereco) references Endereco(id),
+foreign key (id_campanha) references Campanhas(id)
+);
+
+create table Endereco(
+id int primary key auto_increment,
+cep varchar(10) not null,
+rua_aven varchar(255) not null,
+numero_casa varchar(8) not null,
+bairro varchar(200) not null
+);
+
+CREATE TABLE tb_usuario (
+	id int primary key auto_increment not null,
+    usuario varchar(255) not null,
+    senha varchar(255) not null
+);
+
+create table Campanhas(
+id int primary key auto_increment,
+imagem varchar(255),
+nome_campanha varchar(255),
+como_prevenir varchar(255),
+descricao varchar(255)
+);
--- BACKEND/src/rotas.js
+++ BACKEND/src/rotas.js
@@ -1,7 +1,11 @@
 import express from 'express'
-
+import usuarioController from './controller/usuarioController'
 const router = express.Router()
 
+
+export  function AdicionarRotas(servidor){
+    servidor.use(usuarioController);
+}
 
 router.get('/teste', (req, res) => {
   res.json({ mensagem: 'Rota funcionando com sucesso!' })
